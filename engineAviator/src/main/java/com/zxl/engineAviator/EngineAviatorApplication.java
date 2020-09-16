1c18807956afe5702c3a7210deba3540ebb7ff61
zhangxiaolin <953519816@qq.com>
2020-09-16 15:09:09 +0800

    项目初始化

---
+++ engineAviator/src/main/java/com/zxl/engineAviator/EngineAviatorApplication.java
@@ -0,0 +1,13 @@
+package com.zxl.engineAviator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineAviatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngineAviatorApplication.class, args);
+	}
+
+}
---
+++ engineAviator/src/test/java/com/zxl/engineAviator/EngineAviatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxl.engineAviator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineAviatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
